fbc57e1638c3041c3e516e42f6c7f8b667946867
Richard Hadden <richard.hadden@oeaw.ac.at>
2021-05-03 16:00:43 +0200

    save

--- apis_ipif_solr/settings.py
+++ apis_ipif_solr/settings.py
@@ -1,1 +1,7 @@
 STATIC_URL = "/static/"
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": ["templates"],
+    },
+]
--- apis_ipif_solr/urls.py
+++ apis_ipif_solr/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from django.conf.urls.static import static
-from .settings import STATIC_URL
 from .api_views import PersonListView
 
 app_name = "apis_ipif_solr"
 
-urlpatterns = [path("person/", PersonListView.as_view()),] + static(STATIC_URL)
+urlpatterns = [
+    path("person/", PersonListView.as_view()),
+]
